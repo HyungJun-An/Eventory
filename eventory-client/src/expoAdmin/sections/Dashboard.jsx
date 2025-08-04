1d525004d7e84db2e4d267be6f30891fd458896a
Yujin Kim <riniuizoz@naver.com>
2025-08-04 16:37:21 +0900

    refactor: 박람회  관리자 대시보드 UI 파일 수정

---
+++ eventory-client/src/expoAdmin/sections/Dashboard.jsx
@@ -0,0 +1,229 @@
+import React from "react";
+import { WebsiteLogos } from "../../components/WebsiteLogos";
+import "../../assets/css/Dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <div className="div-wrapper" data-model-id="11194:16485">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="group">
+              <div className="weekly-activity">
+                <div className="text-wrapper">예약 현황</div>
+
+                <div className="group-wrapper">
+                  <div className="overlap-group-wrapper">
+                    <div className="div">
+                      <div className="overlap-2">
+                        <div className="text-wrapper-2">일별</div>
+                      </div>
+
+                      <div className="text-wrapper-3">주별</div>
+
+                      <div className="text-wrapper-4">월별</div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="group-2">
+                  <div className="overlap-3">
+                    <div className="text-wrapper-5">CSV 다운로드</div>
+                  </div>
+                </div>
+
+                <div className="group-3">
+                  <div className="overlap-4">
+                    <div className="text-wrapper-6">엑셀 다운로드</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="expense-stats">
+                <div className="text-wrapper">티켓 종류별 예약 비율</div>
+
+                <img
+                  className="group-4"
+                  alt="Group"
+                  src="https://c.animaapp.com/mdwrp9urLhB6f1/img/group-399.png"
+                />
+              </div>
+            </div>
+
+            <div className="group-5">
+              <div className="group-6">
+                <div className="group-7">
+                  <div className="group-8">
+                    <div className="text-wrapper-7">5.80%</div>
+
+                    <div className="text-wrapper-8">입장률</div>
+                  </div>
+
+                  <img
+                    className="group-9"
+                    alt="Group"
+                    src="https://c.animaapp.com/mdwrp9urLhB6f1/img/group-307.png"
+                  />
+                </div>
+              </div>
+
+              <div className="group-10">
+                <div className="group-11">
+                  <div className="group-12">
+                    <div className="text-wrapper-9">1,250</div>
+
+                    <div className="text-wrapper-10">총 예약 수</div>
+                  </div>
+
+                  <img
+                    className="group-9"
+                    alt="Group"
+                    src="https://c.animaapp.com/mdwrp9urLhB6f1/img/group-305.png"
+                  />
+                </div>
+              </div>
+
+              <div className="group-13">
+                <div className="group-14">
+                  <div className="group-15">
+                    <div className="text-wrapper-11">150,000</div>
+
+                    <div className="text-wrapper-12">페이지 조회 수</div>
+                  </div>
+
+                  <img
+                    className="group-9"
+                    alt="Group"
+                    src="https://c.animaapp.com/mdwrp9urLhB6f1/img/group-303.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="overlap-5">
+            <header className="header">
+              <div className="overlap-6">
+                <div className="group-16">
+                  <div className="group-17">
+                    <div className="text-wrapper-13">대시보드</div>
+                  </div>
+
+                  <div className="logo">
+                    <WebsiteLogos
+                      className="website-logos-instance"
+                      color="primary"
+                      removebgPreviewClassName="design-component-instance-node"
+                      removebgPreviewClassNameOverride="website-logos-2"
+                      size="small"
+                    />
+                  </div>
+                </div>
+
+                <div className="group-18">
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-14">로그아웃</div>
+                  </div>
+                </div>
+
+                <div className="group-19">
+                  <div className="overlap-7">
+                    <img
+                      className="vector"
+                      alt="Vector"
+                      src="https://c.animaapp.com/mdwrp9urLhB6f1/img/vector.svg"
+                    />
+
+                    <div className="text-wrapper-15">박람회 선택</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="rectangle" />
+            </header>
+
+            <div className="sidebar-menu">
+              <div className="group-20">
+                <div className="group-21">
+                  <img
+                    className="img-2"
+                    alt="Transfer"
+                    src="https://c.animaapp.com/mdwrp9urLhB6f1/img/transfer-1.svg"
+                  />
+
+                  <div className="text-wrapper-16">예약 관리</div>
+                </div>
+
+                <div className="group-22">
+                  <img
+                    className="img-2"
+                    alt="Economic investment"
+                    src="https://c.animaapp.com/mdwrp9urLhB6f1/img/economic-investment-1.svg"
+                  />
+
+                  <div className="text-wrapper-17">부스 관리</div>
+                </div>
+
+                <div className="group-23">
+                  <img
+                    className="credit-card"
+                    alt="Credit card"
+                    src="https://c.animaapp.com/mdwrp9urLhB6f1/img/credit-card-1.svg"
+                  />
+
+                  <div className="text-wrapper-18">매출 분석</div>
+                </div>
+
+                <div className="group-24">
+                  <img
+                    className="img-2"
+                    alt="Loan"
+                    src="https://c.animaapp.com/mdwrp9urLhB6f1/img/loan-1.svg"
+                  />
+
+                  <div className="text-wrapper-19">정산 관리</div>
+                </div>
+
+                <div className="group-25">
+                  <img
+                    className="img-2"
+                    alt="Service"
+                    src="https://c.animaapp.com/mdwrp9urLhB6f1/img/service-1.svg"
+                  />
+
+                  <div className="text-wrapper-20">환불 처리</div>
+                </div>
+
+                <div className="group-26">
+                  <div className="text-wrapper-21">콘텐츠 관리</div>
+
+                  <img
+                    className="img-2"
+                    alt="User"
+                    src="https://c.animaapp.com/mdwrp9urLhB6f1/img/user-3-1.svg"
+                  />
+                </div>
+
+                <div className="group-27">
+                  <img
+                    className="home"
+                    alt="Home"
+                    src="https://c.animaapp.com/mdwrp9urLhB6f1/img/home-2.svg"
+                  />
+
+                  <div className="text-wrapper-22">대시보드</div>
+
+                  <div className="rectangle-2" />
+                </div>
+              </div>
+
+              <div className="rectangle-3" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
