d4d1fc0b45d664dd7823a8553998daba703625c7
Yujin Kim <riniuizoz@naver.com>
2025-08-05 14:01:04 +0900

    feat: 환불 페이지 구성 파일(css, jsx) 추가

--- eventory-client/src/App.jsx
+++ eventory-client/src/App.jsx
@@ -1,19 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './assets/css/App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./assets/css/App.css";
 
-import RevenuePage from './expoAdmin/RevenuePage';
-import PaymentPage from './expoAdmin/PaymentPage';
+import RevenuePage from "./expoAdmin/RevenuePage";
+import PaymentPage from "./expoAdmin/PaymentPage";
+import RefundPage from "./expoAdmin/RefundPage";
 
 function App() {
-
   return (
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
       <Routes>
         <Route path="/hi" element={<RevenuePage />} />
         <Route path="/hello" element={<PaymentPage />} />
+        <Route path="/refund" element={<RefundPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ eventory-client/src/expoAdmin/sections/RefundGroupWrapper.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../../assets/css/RefundGroupWrapper.css";
+
+const RefundGroupWrapper = () => {
+  return (
+    <div className="view">
+      <div className="text-wrapper-26">전체 내역</div>
+
+      <div className="text-wrapper-27">환불 대기</div>
+
+      <div className="text-wrapper-28">환불 완료</div>
+
+      <div className="overlap-3">
+        <div className="rectangle-12" />
+
+        <div className="rectangle-13" />
+      </div>
+    </div>
+  );
+};
+
+export default RefundGroupWrapper;
