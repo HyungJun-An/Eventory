40915cb49fd8ce1367e9565a97bd79bfd2dc42b8
ddolly518 <20211109@sungshin.ac.kr>
2025-08-05 10:13:23 +0900

    feat: Expo Entity 추가

--- eventory-server/src/main/java/com/eventory/expoAdmin/controller/ExpoAdminController.java
+++ eventory-server/src/main/java/com/eventory/expoAdmin/controller/ExpoAdminController.java
@@ -1,11 +1,13 @@
 package com.eventory.expoAdmin.controller;
 
+import com.eventory.expoAdmin.dto.ExpoResponseDto;
 import com.eventory.expoAdmin.service.ExpoAdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -14,5 +16,11 @@ public class ExpoAdminController {
 
     private final ExpoAdminService expoAdminService;
 
-    
+    // 해당 박람회 관리자에 속하는 전체 박람회 목록
+    public ResponseEntity<List<ExpoResponseDto>> findAllExpos() {
+        List<ExpoResponseDto> expos = expoAdminService.findAllExpos();
+        return ResponseEntity.ok(expos);
+    }
+
+
 }
--- eventory-server/src/main/java/com/eventory/expoAdmin/service/ExpoAdminServiceImpl.java
+++ eventory-server/src/main/java/com/eventory/expoAdmin/service/ExpoAdminServiceImpl.java
@@ -1,12 +1,20 @@
 package com.eventory.expoAdmin.service;
 
+import com.eventory.expoAdmin.dto.ExpoResponseDto;
 import com.eventory.expoAdmin.repository.ExpoAdminRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ExpoAdminServiceImpl implements ExpoAdminService {
 
     private final ExpoAdminRepository expoAdminRepository;
+
+    @Override
+    public List<ExpoResponseDto> findAllExpos() {
+        return null;
+    }
 }
