a0839d1c4799292ccd12b0289478b6b419ca6c97
nyangnyangTimo <gusgo200@naver.com>
2025-08-07 23:28:44 +0900

    FEAT : 로그인 구현

--- eventory-server/src/main/java/com/eventory/auth/controller/AuthController.java
+++ eventory-server/src/main/java/com/eventory/auth/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.eventory.auth.controller;
 
+import com.eventory.auth.dto.LoginRequest;
+import com.eventory.auth.dto.LoginResponse;
 import com.eventory.auth.dto.SignupRequest;
 import com.eventory.auth.dto.SignupResponse;
 import com.eventory.auth.service.AuthService;
@@ -15,9 +17,24 @@ public class AuthController {
 
     private final AuthService authService;
 
-    // 회원가입
+    /**
+     * 사용자 회원가입 요청을 처리
+     * @param request 회원가입 정보
+     * @return 가입 성공 메시지 또는 사용자 정보
+     */
     @PostMapping("/signup")
     public ResponseEntity<SignupResponse> signup(@RequestBody @Valid SignupRequest request) {
         return ResponseEntity.ok(authService.signup(request));
+    }
+
+    /**
+     * 사용자 로그인 요청을 처리
+     * @param request 로그인 요청 정보 (아이디, 비밀번호)
+     * @return 액세스 토큰, 리프레시 토큰 등의 로그인 응답
+     */
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
+        LoginResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
     }
 }
