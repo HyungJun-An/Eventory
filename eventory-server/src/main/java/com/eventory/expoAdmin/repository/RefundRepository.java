33bbd516359eeef6105a164f777f111624aa071e
ddolly518 <20211109@sungshin.ac.kr>
2025-08-08 16:04:49 +0900

    feat: repository 어노테이션 추가

--- eventory-server/src/main/java/com/eventory/expoAdmin/repository/RefundRepository.java
+++ eventory-server/src/main/java/com/eventory/expoAdmin/repository/RefundRepository.java
@@ -4,9 +4,11 @@ import com.eventory.common.entity.Refund;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RefundRepository extends JpaRepository<Refund, Long> {
     @Query("""
     SELECT COUNT(r)
