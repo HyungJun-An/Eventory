64055f643e3e7c8d24514d7463a8c301f4efcf5c
Jeon <hyojin890911@naver.com>
2025-08-08 10:09:21 +0900

    [Entity] ERD 수정 내용 반영

--- eventory-server/src/main/java/com/eventory/common/entity/BannerClickLog.java
+++ eventory-server/src/main/java/com/eventory/common/entity/BannerClickLog.java
@@ -21,6 +21,6 @@ public class BannerClickLog {
     @JoinColumn(name = "banner_id")
     private Banner banner;
 
-    @Column(name = "click_at", nullable = false)
-    private Long clickAt;
+    @Column(name = "click_count", nullable = false)
+    private Long clickCount;
 }
