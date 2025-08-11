23e28ba13ef1909b96db499919003369ed770db7
ddolly518 <20211109@sungshin.ac.kr>
2025-08-11 10:23:56 +0900

    feat: 검증 어노테이션 추가

--- eventory-server/src/main/java/com/eventory/expoAdmin/dto/PaymentResponseDto.java
+++ eventory-server/src/main/java/com/eventory/expoAdmin/dto/PaymentResponseDto.java
@@ -1,6 +1,9 @@
 package com.eventory.expoAdmin.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,12 +15,25 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 public class PaymentResponseDto {
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String code;
+
+    @NotNull
+    @Positive
     private Integer people;
+
+    @NotBlank
     private String method;
+
+    @NotNull
+    @Positive
     private BigDecimal amount;
 
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime paidAt;
 }
