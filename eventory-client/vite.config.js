8fe28485cbd429b33d674d804b96ba297b884404
안형준 <anhyeongjun@anhyeongjun-ui-MacBookAir.local>
2025-08-14 11:30:38 +0900

    hotfix: publicDir변경

--- eventory-client/vite.config.js
+++ eventory-client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     mode === "development" && screenGraphPlugin(),
   ].filter(Boolean),
 
-  publicDir: ".",
+  publicDir: "./static",
   base: "/",
 
   server: {
